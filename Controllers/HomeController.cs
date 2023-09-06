8940451cf987e19a222c1e360cac09878429ce94
Eseoghene Okoro <enerhen2013@gmail.com>
2023-09-06 15:07:59 +0100

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EnvTest.Models;
+
+namespace EnvTest.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index(Phrase phrase)
+    {
+        var word = Environment.GetEnvironmentVariable("MY_VAR");
+        if(word != null)
+        {
+            phrase.MyVar = word;
+        }
+        return View(phrase);
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Phrase.cs
@@ -0,0 +1,6 @@
+namespace EnvTest.Models;
+
+public class Phrase
+{
+    public string? MyVar { get; set; }
+}
